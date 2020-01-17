2b19c4d4e341e474e615fceef5346816d11b1f81
alexovonathan22 <avo.nathan@gmail.com>
2020-01-17 10:40:49 +0100

    add filter functionality

--- src/app/customers/customers-list/filter-textbox.component.ts
+++ src/app/customers/customers-list/filter-textbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-filter-textbox',
@@ -8,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
     <div class="input-group-prepend">
       <span class="input-group-text" id="">Filter</span>
     </div>
-    <input type="text" class="form-control">
+    <input type="text" class="form-control" [(ngModel)]="filter">
     
   </div>
     `
 })
 export class FilterTextboxComponent implements OnInit {
+    
+     
+  private _filter: string;
+  @Input() get filter() {
+      return this._filter;
+  }
+  
+  set filter(val: string) { 
+      this._filter = val;
+      this.changed.emit(this.filter); //Raise changed event
+  }
+
+  @Output() changed: EventEmitter<string> = new EventEmitter<string>();
+  
+
     constructor() {}
     
     ngOnInit() {
